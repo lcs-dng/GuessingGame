15a2a0069bae3fa176512eebb0c32bc41688a525
Dexter Ng <DNg@lcs.on.ca>
2024-02-02 08:58:08 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Dexter Ng on 2024-02-02.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
