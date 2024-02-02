734f3196cde146f60d4dc82636d78ed97a947573
Dexter Ng <DNg@lcs.on.ca>
2024-02-02 09:01:28 -0500

    Re-organized project and created a GameView file.

---
+++ GuessingGame/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  GameView.swift
+//  GuessingGame
+//
+//  Created by Dexter Ng on 2024-02-02.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameView()
+}
